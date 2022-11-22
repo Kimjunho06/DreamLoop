3818c4e9370e5e09c3e0315fe2707444e8b979a5
KimCda <kjhlove0119@naver.com>
2022-11-23 00:49:22 +0900

    dasdasf

---
+++ Assets/02_Scripts/UI/OptionButton.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class OptionButton : MonoBehaviour
+{
+    public Image _panel;
+    public Image _panelBackGround;
+
+    public Button _optionOnBtn;
+    public Button _optionOffBtn;
+
+    public void OnPanelOpen()
+    {
+        Sequence seq = DOTween.Sequence();
+        _optionOnBtn.gameObject.SetActive(false);
+        seq.AppendCallback(PanelOnActive);
+        seq.Append(_panel.DOFade(0.2f, 2f));
+        seq.AppendCallback(PanelBackOnActive);
+        seq.Append(_panelBackGround.transform.DOScale(1, 1f).SetEase(Ease.OutBounce));
+        seq.AppendCallback(OptionCloseBtnOnActive);
+    }
+
+    public void OnPanelClose()
+    {
+        Sequence seq = DOTween.Sequence();
+        _optionOffBtn.gameObject.SetActive(false);
+        seq.Append(_panelBackGround.transform.DOScale(0, 1f).SetEase(Ease.OutBounce));
+        seq.AppendCallback(PanelBackOffActive);
+        seq.Append(_panel.DOFade(0, 2f));
+        seq.AppendCallback(PanelOffActive);
+        seq.AppendCallback(OptionOpenBtnOnActive);
+    }
+
+    private void PanelOnActive() => _panel.gameObject.SetActive(true);
+    private void PanelOffActive() => _panel.gameObject.SetActive(false);
+    private void PanelBackOnActive() => _panelBackGround.gameObject.SetActive(true);
+    private void PanelBackOffActive() => _panelBackGround.gameObject.SetActive(false);
+
+    private void OptionOpenBtnOnActive() => _optionOnBtn.gameObject.SetActive(true);
+    private void OptionOpenBtnOffActive() => _optionOnBtn.gameObject.SetActive(false);
+    private void OptionCloseBtnOnActive() => _optionOffBtn.gameObject.SetActive(true);
+    private void OptionCloseBtnOffActive() => _optionOffBtn.gameObject.SetActive(false);
+
+}
